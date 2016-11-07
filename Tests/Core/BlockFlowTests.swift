48cca86c05150006312bb0f161b654da5ec6ce7b
JARMourato <joao.armourato@gmail.com>
2016-11-07 14:54:04 +0000

    changed timeout value on test

--- Tests/Core/BlockFlowTests.swift
+++ Tests/Core/BlockFlowTests.swift
@@ -52,7 +52,7 @@ class BlockFlowTests: XCTestCase {
             print("Error")
         }.start()
 
-        waitForExpectations(timeout: 1, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
     }
 
     func testNoBlocksFunctionality() {
